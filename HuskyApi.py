4e74be3c676470f8d7fcf610274d17d145713dbe
EC2 Default User <ec2-user@ip-172-31-6-157.us-west-2.compute.internal>
2021-06-29 20:20:09 +0000

    Husky Shelters API

---
+++ HuskyApi.py
@@ -0,0 +1,77 @@
+from pprint import pprint
+from decimal import Decimal
+import json
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+accounts_table = os.environ['ACCOUNTS-TABLE']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(accounts_table)
+
+def addUser(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    account_id = path.split("/")[-1] # ["account", "id"]
+    
+    body = json.loads(event["body"])
+    print(body)
+    print(account_id)
+    item = {
+        'pk': account_id,
+        'name': body["name"],
+        'doggy': body["doggy"],
+        'race': body["race"],
+        'age': body["age"]
+    }
+    print(json.dumps(item))
+    table.put_item(
+       Item=item
+    )
+    
+def getUser(pk):
+    
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Dogs')
+    
+    try:
+        response = table.get_item(Key={'pk': pk})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response['Item']
+    
+    
+def setUser(pk, name, doggy, race, age):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Dogs')
+
+    response = table.update_item(
+        Key={
+            'pk': pk,
+            'name': name,
+            'doggy':doggy,
+            'race': race,
+            'age': age
+        },
+        UpdateExpression="set info.age=:a, info.name=:n, info.doggy=:d, info.race =:r",
+        ExpressionAttributeValues={
+            ':a': Decimal(age),
+            ':n': name,
+            ':d': doggy,
+            ':r': race
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return response
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
